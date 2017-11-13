23da24ba05c2c5a3ed0fa029a96be7c010d0f94e
Scott Atkins <satkins16@gmail.com>
2017-11-13 14:06:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Driver {
     store.drivers.push(this)
   }
   trips() {
-    return store.trips.filter(trip => {
+    return store.trips.filter((trip) => {
       return trip.driverId === this.id
     })
   }
